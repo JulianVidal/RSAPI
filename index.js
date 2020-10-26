586ae28d648989219e800b9a0252cea0147d5d0e
JulianVidal <49546430+spiderpiggy03@users.noreply.github.com>
2020-10-26 21:57:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express =  require('express')
 
 const app = express()
 
+
 async function scrape() {
   const browser = await puppeteer.launch({headless: false})
 
@@ -13,23 +14,31 @@ async function scrape() {
     waitUntil: 'domcontentloaded'
   })
 
-  // Types search
+  await page.evaluate( async () => {
+    let distance = 0
+    while (distance < document.body.scrollHeight) {
+      await new Promise(resolve => setTimeout(resolve, 250));
+      window.scrollBy(0, window.innerHeight)
+      distance += window.innerHeight
+    }
+    })
+
   const list = await page.evaluate( () => {
     const properties = []
 
     const propertiesList = document.querySelector('#grid-search-results > ul')
-    const porperty = propertiesList.childNodes[1]
 
     for (let i = 0; i < propertiesList.childNodes.length; i++) {
       const property = propertiesList.childNodes[i];
-
-      const adress = porperty.querySelector('address').innerText
-      const link = porperty.querySelector('a').href
-      const price = porperty.querySelector('.list-card-price').innerText
-      const beds = porperty.querySelector('.list-card-details').childNodes[0].innerText
-      const baths = porperty.querySelector('.list-card-details').childNodes[1].innerText
-      const sqft = porperty.querySelector('.list-card-details').childNodes[2].innerText
-      const image = porperty.querySelector('img').src
+      console.log(i, property)
+      if (i === 2) continue
+      const adress = property.querySelector('address').innerText
+      const link = property.querySelector('a').href
+      const price = property.querySelector('.list-card-price').innerText
+      const beds = property.querySelector('.list-card-details').childNodes[0].innerText
+      const baths = property.querySelector('.list-card-details').childNodes[1].innerText
+      const sqft = property.querySelector('.list-card-details').childNodes[2].innerText
+      const image = property.querySelector('img').src
       
       properties.push({adress, link, price, beds, baths, sqft, image})
     }
