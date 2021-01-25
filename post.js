dd6bcf3da99b0047f85a237ccf386c4538da9cff
JulianVidal <49546430+spiderpiggy03@users.noreply.github.com>
2021-01-25 09:17:34 +0300

    Added login

--- post.js
+++ post.js
@@ -11,7 +11,7 @@ app.get('/post', (req, res) => {
   res.send('Hello!')
 })
 
-app.post('/post', (req, res) => {
+app.post('/signup', (req, res) => {
   const user = req.body
   console.log(req.body)
 
@@ -24,20 +24,43 @@ app.post('/post', (req, res) => {
       const repeated = users.find(u => u.email === user.email)
       console.log(repeated, ' repeated')
       if (repeated === undefined) {
-        console.log('1')
+        console.log('1', 'singup')
         users.push(user)
         const json = JSON.stringify(users)
         fs.writeFile('./data/users.json', json, error => {if (error) throw error})
         res.sendStatus(200)
       } else {
-        console.log('2')
-        res.status(406).send({message:'Already Signed Up'})
+        console.log('2', 'signup')
+        res.status(406).send({message:'Account already exists'})
       }
 
     }
   } )
 })
 
+app.post('/login', (req, res) => {
+  const user = req.body
+  console.log(req.body)
+
+  fs.readFile('./data/users.json', 'utf8', (err, data) => {
+    if (err) {
+      return true
+    } else {
+      const users = JSON.parse(data)
+
+      const repeated = users.find(u => u.email === user.email)
+      console.log(repeated, ' repeated')
+      if (repeated !== undefined) {
+        console.log('1', 'Login')
+        res.sendStatus(200)
+      } else {
+        console.log('2', 'login')
+        res.status(406).send({message:'Account Does Not Exist'})
+      }
+
+    }
+  } )
+})
 
 
 app.listen(5000)
