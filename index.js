f09d699f92bbd60261161819cd1352614740d92c
JulianVidal <49546430+spiderpiggy03@users.noreply.github.com>
2022-10-16 21:41:17 +0100

    Fixed api url

--- index.js
+++ index.js
@@ -31,17 +31,14 @@ app.get("/properties", async (req, res) => {
   const search = req.query.location;
   const option = req.query.type;
   let error;
-  const data = await fetch(
-    "https://realtor.p.rapidapi.com/properties/v2/list-for-" +
-      option +
-      "?city=" +
-      search +
-      "&limit=100&offset=0&sort=relevance",
+
+  let auto_complete = await fetch(
+    'https://realtor.p.rapidapi.com/locations/v2/auto-complete?input=' + search + '&limit=10',
     {
       method: "GET",
       headers: {
-        "x-rapidapi-key": "faace970c9mshb9a6dc176f9b095p1b3796jsn1ac808ba8436",
-        "x-rapidapi-host": "realtor.p.rapidapi.com",
+        'X-RapidAPI-Key': 'faace970c9mshb9a6dc176f9b095p1b3796jsn1ac808ba8436',
+        'X-RapidAPI-Host': 'realtor.p.rapidapi.com'
       },
     }
   )
@@ -51,20 +48,58 @@ app.get("/properties", async (req, res) => {
     .catch((err) => {
       error = new Error(err);
     });
-  console.log(data);
+  auto_complete = auto_complete.autocomplete;
+  if (auto_complete === undefined || auto_complete.length === 0) {res.json({ data:undefined, error:new Error("Auto Complete fail") });return;}
+  else auto_complete = auto_complete[0];
+
+  // 'https://realtor.p.rapidapi.com/properties/v2/list-for-sale?city=Washington&limit=200&offset=0&sort=relevance'
+  //  https://realtor.p.rapidapi.com/properties/v2/list-for-sale?city=Washington&limit=200&offset=0&sort=relevance undefined
+  // 'https://realtor.p.rapidapi.com/properties/v2/list-for-sale?city=New%20York%20City&limit=200&state_code=NY&offset=0&sort=relevance'
+  const data = await fetch(
+      "https://realtor.p.rapidapi.com/properties/v2/list-for-" +
+      option +
+      "?city=" +
+      search +
+      "&state_code=" +
+      auto_complete.state_code +
+      "&limit=200&offset=0&sort=relevance"
+      ,
+    {
+      method: "GET",
+      headers: {
+        'X-RapidAPI-Key': 'faace970c9mshb9a6dc176f9b095p1b3796jsn1ac808ba8436',
+        'X-RapidAPI-Host': 'realtor.p.rapidapi.com'
+      },
+    }
+  )
+    .then(async (response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      error = new Error(err);
+    });
+    
+  console.log("https://realtor.p.rapidapi.com/properties/v2/list-for-" +
+      option +
+      "?city=" +
+      search +
+      "&state_code=" +
+      auto_complete.state_code +
+      "&limit=200&offset=0&sort=relevance",data);
   res.json({ data, error });
 });
 
 app.get("/property", async (req, res) => {
   const propertyID = req.query.property_id;
   let error;
+
   const data = await fetch(
     "https://realtor.p.rapidapi.com/properties/v2/detail?property_id=" +
       propertyID,
     {
       method: "GET",
       headers: {
-        "x-rapidapi-key": "faace970c9mshb9a6dc176f9b095p1b3796jsn1ac808ba8436",
+        "x-rapidapi-key": "c4608e73f8msh2cc5c53b3a9e8bbp1dcb3ajsn81d02860c74d",
         "x-rapidapi-host": "realtor.p.rapidapi.com",
       },
     }
@@ -79,4 +114,4 @@ app.get("/property", async (req, res) => {
   res.json({ data, error });
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 4000);
